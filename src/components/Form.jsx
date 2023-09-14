bc2fe41afded25cafd3d187a16428df115427c93
Lisa ^^ <islahola>
2023-09-14 10:00:15 +0700

    required

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -7,10 +7,9 @@ const Form = () => {
     const form = useRef();
     const sendEmail = (e) => {
       e.preventDefault();
-  
       emailjs.sendForm('service_l273w57', 'template_ed2toyw', form.current, 'TGboJ_c8r3cxdla0j')
         .then((result) => {
-            alert('ok')
+            alert('Terimakasih Pesan Anda terkirim')
             console.log(result.text);
         }, (error) => {
             console.log(error.text);
@@ -32,25 +31,25 @@ const Form = () => {
           Jangan ragu untuk berdiskusi dengan pakar kami untuk mengoptimalkan critical workload bisnis Anda.
           </p>
           <div className="w-full max-w-lg">
-            <form className=" shadow-md rounded px-8 pt-6 pb-8 mb-4 mt-5" ref={form} >
+            <form className=" shadow-md rounded px-8 pt-6 pb-8 mb-4 mt-5" ref={form} onSubmit={sendEmail}>
                 <div className="mb-4">
                     <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="nama">
                         Nama
                     </label>
-                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="nama" type="text" placeholder="Nama" name="name"/>
+                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="nama" type="text" placeholder="Nama" name="name" required/>
                 </div>
                <div className="flex justify-between">
                <div className="mb-4">
                     <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="jabatan">
                         Jabatan
                     </label>
-                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="jabatan" type="text" placeholder="Jabatan" name="jabatan"/>
+                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="jabatan" type="text" placeholder="Jabatan" name="jabatan" required/>
                 </div>
                 <div className="mb-4 ml-5 md:ml-0">
                     <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="divisi">
                         Divisi
                     </label>
-                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="divisi" type="text" placeholder="divisi" name="divisi"/>
+                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="divisi" type="text" placeholder="divisi" name="divisi" required/>
                 </div>
                </div>
              <div className="flex justify-between">
@@ -58,13 +57,13 @@ const Form = () => {
                     <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="company">
                         Company
                     </label>
-                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="company" type="text" placeholder="company" name="company"/>
+                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="company" type="text" placeholder="company" name="company" required/>
                 </div>
                 <div className="mb-4 ml-5 md:ml-0">
                     <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="telp">
                         No.Telepon
                     </label>
-                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="telp" type="text" placeholder="telp" name="phone"/>
+                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="telp" type="text" placeholder="telp" name="phone" required/>
                 </div>
              </div>
             
@@ -72,7 +71,7 @@ const Form = () => {
                     <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
                         Email
                     </label>
-                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="text" placeholder="email" name="email"/>
+                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="text" placeholder="email" name="email" required/>
                 </div>
                 <div className="mb-4">
                     <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="kebutuhan">
@@ -91,7 +90,7 @@ const Form = () => {
                 </div>
                 <div className="flex items-center justify-between">
                 
-                <button onClick={sendEmail} className="py-2 px-6 font-poppins font-medium text-[18px] text-primary rounded-[10px]  bg-blue-gradient outline-none" type="button">
+                <button className="py-2 px-6 font-poppins font-medium text-[18px] text-primary rounded-[10px]  bg-blue-gradient outline-none" type="submit">
                   Submit
                 </button>
                 </div>
