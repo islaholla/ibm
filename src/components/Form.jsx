9d9e8776cf366a51098b7b07d95a28e12acf7e84
Lisa ^^ <islahola>
2023-09-14 10:42:22 +0700

    required

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -34,20 +34,20 @@ const Form = () => {
             <form className=" shadow-md rounded px-8 pt-6 pb-8 mb-4 mt-5" ref={form} onSubmit={sendEmail}>
                 <div className="mb-4">
                     <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="nama">
-                        Nama
+                        Nama*
                     </label>
                     <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="nama" type="text" placeholder="Nama" name="name" required/>
                 </div>
                <div className="flex justify-between">
                <div className="mb-4">
                     <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="jabatan">
-                        Jabatan
+                        Jabatan*
                     </label>
                     <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="jabatan" type="text" placeholder="Jabatan" name="jabatan" required/>
                 </div>
                 <div className="mb-4 ml-5 md:ml-0">
                     <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="divisi">
-                        Divisi
+                        Divisi*
                     </label>
                     <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="divisi" type="text" placeholder="divisi" name="divisi" required/>
                 </div>
@@ -55,13 +55,13 @@ const Form = () => {
              <div className="flex justify-between">
              <div className="mb-4">
                     <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="company">
-                        Company
+                        Company*
                     </label>
                     <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="company" type="text" placeholder="company" name="company" required/>
                 </div>
                 <div className="mb-4 ml-5 md:ml-0">
                     <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="telp">
-                        No.Telepon
+                        No.Telepon*
                     </label>
                     <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="telp" type="text" placeholder="telp" name="phone" required/>
                 </div>
@@ -69,27 +69,32 @@ const Form = () => {
             
                 <div className="mb-4">
                     <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
-                        Email
+                        Email*
                     </label>
                     <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="text" placeholder="email" name="email" required/>
                 </div>
                 <div className="mb-4">
-                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="kebutuhan">
-                        kebutuhan
+                    <label className="block text-gray-700 text-sm font-bold mb-2" >
+                        Kebutuhan*
                     </label>
                    <div className="flex">
                    <div>
-                        <input type="checkbox" id="some_id" name= "meeting" value="Meeting" />
+                        <input type="checkbox" id="meeting" name= "meeting" value="Meeting" />
                         < label className="ml-5 text-white" htmlFor="meeting">Meeting</label>
                     </div>
                     <div className="ml-5">
-                        <input type="checkbox" id="some_id" name="demo" value="Lihat Demo"/>
+                        <input type="checkbox" id="demo" name="demo" value="Lihat Demo"/>
                         < label className="ml-5 text-white" htmlFor="demo" >Lihat demo</label>
                     </div>
                    </div>
                 </div>
-                <div className="flex items-center justify-between">
-                
+                <div>
+                <div className="mb-4">
+                    <div className="">
+                        < label className="text-white  text-[12px] leading-[15px]" htmlFor="privasi"><p className="text-justify">*Wajib diisi </p></label>
+                        < label className="ml-5 text-white  text-[12px] leading-[15px]" htmlFor="privasi"><p className="text-justify">Dengan klik submit, saya mengonfirmasi bahwa saya menginginkan informasi dan penawaran tambahan terkait produk dan layanan Mastersystem. Saya juga menyatakan bahwa saya telah membaca dan menyetujui <a className="text-blue-300" href="https://www.mastersystem.co.id/privacy-policy">kebijakan privasi Mastersystem</a></p></label>
+                    </div>
+                </div>
                 <button className="py-2 px-6 font-poppins font-medium text-[18px] text-primary rounded-[10px]  bg-blue-gradient outline-none" type="submit">
                   Submit
                 </button>
